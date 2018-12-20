be0e930e03ec65fdf3210c6ef62d90d54c8f36e5
stulzq <stulzq@qq.com>
2018-12-20 17:38:01 +0800

    [Add[ imagesharp test

---
+++ src/imagesharp-test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace imagesharp_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Image.Load(string path) is a shortcut for our default type. 
+            // Other pixel formats use Image.Load<TPixel>(string path))
+            using (Image<Rgba32> image = Image.Load("foo.jpg"))
+            {
+                image.Mutate(x => x
+                    .Resize(image.Width / 2, image.Height / 2)
+                    .Grayscale());
+                image.Save("bar.jpg"); // Automatic encoder selected based on extension.
+            }
+        }
+    }
+}
